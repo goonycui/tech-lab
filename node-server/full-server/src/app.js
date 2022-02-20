3a2140a1224fef6b56bfcc039e7eed53b092016a
goonycui <goonycui@outlook.com>
2022-02-20 22:43:50 +0800

    node express serve html file

--- node-server/full-server/src/app.js
+++ node-server/full-server/src/app.js
@@ -1,6 +1,6 @@
 
 const express = require("express");
-
+const path = require("path");
 const app = express();
 
 module.exports = app;
@@ -11,4 +11,8 @@ app.get("/", (req, res) => {
 
 app.get("/test", (req, res) => {
 	res.send("Testing works!");
+});
+
+app.get("/index", (req, res) => {
+	res.sendFile(path.join(__dirname, "views", "index.html"));
 });
---
+++ node-server/full-server/src/readme.md
@@ -0,0 +1,3 @@
+sendFile(): an Express.js method available on the response object (res) that is used to render a file
+path: a buit-in module that gives us all of the tools we need to handle cross-platform paths.
+__dirname: a global object that return the absolute path of the directory containing the currently executing file
