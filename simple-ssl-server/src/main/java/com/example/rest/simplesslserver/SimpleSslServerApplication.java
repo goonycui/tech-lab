582eb72ae0d2c9e69d5de747fd1aadcc72ce552f
goonycui <goonycui@outlook.com>
2022-02-27 09:30:33 +0800

    spring boot simple ssl server

---
+++ simple-ssl-server/src/main/java/com/example/rest/simplesslserver/SimpleSslServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.rest.simplesslserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSslServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSslServerApplication.class, args);
+	}
+
+}
---
+++ simple-ssl-server/src/test/java/com/example/rest/simplesslserver/SimpleSslServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rest.simplesslserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSslServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
